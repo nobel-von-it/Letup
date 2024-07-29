cf31a89f945e55f9aa3e0835b1e762b5c07020d8
nobel-von-it <maksimdavydenko12@gmail.com>
2024-07-29 20:38:47 +0700

    init

---
+++ flatpak.sh
@@ -0,0 +1,10 @@
+
+flatpak install -y --noninteractive org.videolan.VLC
+flatpak install -y --noninteractive com.valvesoftware.Steam
+flatpak install -y --noninteractive md.obsidian.Obsidian
+flatpak install -y --noninteractive com.obsproject.Studio
+flatpak install -y --noninteractive io.gitlab.zehkira.Monophony
+flatpak install -y --noninteractive org.gimp.GIMP
+flatpak install -y --noninteractive net.ankiweb.Anki
+flatpak install -y --noninteractive com.discordapp.Discord
+flatpak install -y --noninteractive flathub portproton
---
+++ git.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:nobel-von-it/tdrs ~/Dev/Rusty/tdrs
+git clone git@github.com:nobel-von-it/inirs ~/Dev/Rusty/inirs
+git clone git@github.com:nobel-von-it/rust_kata ~/Dev/Rusty/rust_kata
+git clone git@github.com:nobel-von-it/simpvim ~/.config/nvim
+
+git config --global user.email maksimdavydenko12@gmail.com
+git config --global user.name nobel-von-it
