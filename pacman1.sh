203fc8597a5c89bd38cfea37f9b80a10ff00636b
nobel-von-it <maksimdavydenko12@gmail.com>
2024-07-29 23:19:50 +0700

    add tmux

--- pacman1.sh
+++ pacman1.sh
@@ -1,3 +1,3 @@
-sudo pacman -S syncthing flatpak noto-fonts-cjk ttf-jetbrains-mono-nerd neovim xclip dex zsh alacritty neofetch ripgrep eza zed telegram-desktop openssh unzip tar firefox openssh
+sudo pacman -S syncthing tmux flatpak noto-fonts-cjk ttf-jetbrains-mono-nerd neovim xclip dex zsh alacritty neofetch ripgrep eza zed telegram-desktop openssh unzip tar firefox openssh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
